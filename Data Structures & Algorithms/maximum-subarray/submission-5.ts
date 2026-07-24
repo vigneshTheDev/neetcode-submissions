1c6aa00f83cc6a0f1a9286962b47c1b1de8cf3b3
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-07-24 08:53:46 +0530

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.ts
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums: number[]): number {
+        let max = -Infinity
+        let sum = 0
+
+        if (!nums.length) return 0
+
+
+        for (let r = 0; r < nums.length; r++) {
+            sum = Math.max(sum + nums[r], nums[r])
+            max = Math.max(max, sum)
+        }
+        return max;
+    }
+}
