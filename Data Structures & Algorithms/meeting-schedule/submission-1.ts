fa8466608d2a6a73e441a56d6c6fb3599bc01180
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-07-27 10:59:57 +0530

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals: Interval[]): boolean {
+        intervals.sort((a, b) => a.start - b.start)
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i-1].end > intervals[i].start) return false
+        }
+
+        return true
+    }
+}
