04eaed6fcd37c99341ea2460b6d3638f6f4c905b
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-08-22 22:55:20 +0530

    Add: house-robber-ii - submission-7

---
+++ Data Structures & Algorithms/house-robber-ii/submission-7.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        return max(nums[0], self.sub_rob(nums[:-1]), self.sub_rob(nums[1:]))
+
+    def sub_rob(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        one_step_behind = 0
+        two_step_behind = 0
+
+        out = 0
+
+        for i in range(0, len(nums)):
+            amount_if_robbed = two_step_behind + nums[i]
+            amount_if_skipped = one_step_behind
+
+            current = max(amount_if_robbed, amount_if_skipped)
+
+            two_step_behind = one_step_behind
+            one_step_behind = current
+
+            out = current
+
+        return out
