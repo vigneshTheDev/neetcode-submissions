bf1c18a9b721ac92d7d1fd2e5713c3dfe1ff51c8
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-08-26 20:15:11 +0530

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.go
@@ -0,0 +1,9 @@
+func maxProfit(prices []int) int {
+	maxP := 0
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			maxP = max(maxP, prices[j] - prices[i])
+		}
+	}
+	return maxP
+}
