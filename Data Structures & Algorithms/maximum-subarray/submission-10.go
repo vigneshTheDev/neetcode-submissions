18b65ec39d2063f0322e111db20d851d258ee2be
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-08-27 17:44:31 +0530

    Add: maximum-subarray - submission-10

---
+++ Data Structures & Algorithms/maximum-subarray/submission-10.go
@@ -0,0 +1,14 @@
+func maxSubArray(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+    maxSum := nums[0]
+	currSum := 0
+
+	for _, num := range nums {
+		currSum = max(currSum, 0) + num
+		maxSum = max(maxSum, currSum)
+	}
+	return maxSum
+}
