5d738de3b20263dede12048860334606a531f5e4
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-08-28 11:09:47 +0530

    Add: minimum-window-with-characters - submission-14

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-14.go
@@ -0,0 +1,38 @@
+func minWindow(s string, t string) string {
+	l := 0
+	tFreqMap := map[byte]int{}
+	sFreqMap := map[byte]int{}
+
+	minLen := len(s) + 1
+	minLenStr := ""
+
+	for _, c := range []byte(t) {
+		tFreqMap[c]++
+	}
+
+	need := len(tFreqMap)
+	have := 0
+
+	for r := 0; r < len(s); r++ {
+		sFreqMap[s[r]]++
+
+		if sFreqMap[s[r]] == tFreqMap[s[r]] {
+			have++
+		}
+
+		for have == need {
+			if minLen > r - l + 1 {
+				minLen = r - l + 1
+				minLenStr = s[l:r+1]
+			}
+
+			if sFreqMap[s[l]] == tFreqMap[s[l]] {
+				have--
+			}
+			sFreqMap[s[l]]--
+			l++
+		}
+	}
+
+	return minLenStr
+}
