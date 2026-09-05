1c6eac2e8e58a16b120dd050c62d4e7175a2ff33
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-09-05 10:52:12 +0530

    Add: duplicate-integer - submission-18

---
+++ Data Structures & Algorithms/duplicate-integer/submission-18.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        const seen = new Set()
+        for (let n of nums) {
+            if (seen.has(n)) {
+                return true
+            }
+            seen.add(n)
+        }
+        return false
+    }
+}
