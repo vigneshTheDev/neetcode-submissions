a7710d1d156e8006b5ab67e828abb430a911da56
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-09-08 11:01:17 +0530

    Add: string-encode-and-decode - submission-17

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-17.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        const out = []
+        for (let s of strs) {
+            out.push(s.length, s)
+        }
+        return out.join(',')
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        if (str.length === 0) return []
+
+        let i = 0
+        const out = []
+        while (i < str.length) {
+            const len = str.slice(i).match(/([0-9]+),/)[1]
+            const start = i + len.length + 1
+            const end = start + +len
+            out.push(str.slice(start, end))
+            i+= len.length + +len + 2
+        }
+        return out
+    }
+}
