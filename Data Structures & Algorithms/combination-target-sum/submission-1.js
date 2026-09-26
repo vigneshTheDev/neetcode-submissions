aa66a6122f0da5436a58a12755f0dd28a04437e8
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-09-26 22:48:57 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        const res = [];
+        this.dfs(nums, target, 0, [], 0, res);
+        return res;
+    }
+    dfs(nums, target, curr, candidate, total, res) {
+        if (total === target) {
+            return res.push(candidate);
+        }
+
+        if (curr >= nums.length) return;
+        if (total > target) return;
+
+        this.dfs(nums, target, curr, [...candidate, nums[curr]], total + nums[curr], res);
+        this.dfs(nums, target, curr + 1, candidate, total, res);
+    }
+}
