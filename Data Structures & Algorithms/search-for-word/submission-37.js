b414ec0dc376e2b0e6ee4feaf2f99b59d1b56fb3
Vigneshkumar Chinnachamy <thegardenerwhocodes@gmail.com>
2026-09-26 15:54:13 +0530

    Add: search-for-word - submission-37

---
+++ Data Structures & Algorithms/search-for-word/submission-37.js
@@ -0,0 +1,40 @@
+const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+]
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @param {string} word
+     * @return {boolean}
+     */
+    exist(board, word) {
+        const inPath = new Array(board.length).fill(null).map(() => [])
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+                if(this.walk(board, word, [i, j], 0, inPath)) return true
+            }
+        }
+        return false
+    }
+    walk(board, word, curr, wordIdx, inPath) {
+        const [i, j] = curr
+        if (i < 0 || j < 0 || i > board.length - 1 || j > board[0].length - 1) return false
+        if (inPath[i][j]) return false
+        if (word[wordIdx] !== board[i][j]) return false
+        if (word.length - 1 === wordIdx) return true
+
+        inPath[i][j] = true
+        wordIdx++
+        for (let dir of directions) {
+            if (this.walk(board, word, [i + dir[0], j + dir[1]], wordIdx, inPath)) {
+                return true
+            }
+        }
+
+        inPath[i][j] = false
+        return false
+    }
+}
